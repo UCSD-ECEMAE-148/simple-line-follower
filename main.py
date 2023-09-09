44c0af730fc448696279ffd27539789029477de2
Moises Lopez <mdlopezme@gmail.com>
2023-09-09 14:11:10 -0700

    test3

--- main.py
+++ main.py
@@ -9,25 +9,25 @@ camera = OAKDCamera()
 vehicle = VESC()
 detector = DepthFilter(camera)
 
+# 1D array gausian kernel
+array = np.linspace(0, 2.5, 320)
+array = np.concatenate((array, -np.flip(array)), axis=0)
+
 while True:
     depth = camera.get_depth()
 
     depth_norm = depth/255 # Normalize depth to 0-1
     depth_exp = 1 - np.exp(-5*depth_norm) # Apply exponential function
 
-    # Horizontal histogram of the depth image
-    hist = np.sum(depth_exp, axis=0)
+    steer = depth_exp @ array
+    steer = steer[:320]
+    steer = np.sum(steer)/(640*500)
 
-    # Low pass filter, kernel size 5
-    hist = np.convolve(hist, np.ones(5)/5, mode='same')
+    # clamp between -1 to 1
+    steer = np.clip(steer, -1, 1)
 
-    # Get index of smallest value in the histogram
-    steer = np.argmin(hist) - 320
-
-    # Normilize steering value to -1 to 1
-    steer = steer/320
-
-    # Remap steering value to 0 to 1
+    # print(steer)
+    # map to from -1 to 1 to 0 to 1
     steer = (steer + 1)/2
 
     vehicle.run(steer, -0.03)
