5f5fcee4195915ba460d54c0e5db24a4062d16cb
Moises Lopez <mdlopezme@gmail.com>
2023-09-09 13:40:53 -0700

    Linearize model

--- main.py
+++ main.py
@@ -9,26 +9,26 @@ camera = OAKDCamera()
 vehicle = VESC()
 detector = DepthFilter(camera)
 
-# 1D array gausian kernel
-array = np.linspace(0, 2.5, 320)
-array = np.concatenate((array, -np.flip(array)), axis=0)
-
-
-# Negate the second half of the array
-# array = np.concatenate((array[:320], -array[320:]), axis=0)
-
 while True:
     depth = camera.get_depth()
 
-    steer = depth @ array
-    steer = steer[:320]
-    steer = np.sum(steer)/(640*500)
+    depth_norm = depth/255 # Normalize depth to 0-1
+    depth_exp = 1 - np.exp(-5*depth_norm) # Apply exponential function
 
-    # clamp between -1 to 1
+    # Horizontal histogram of the depth image
+    hist = np.sum(depth_exp, axis=0)
+
+    # Low pass filter, kernel size 5
+    hist = np.convolve(hist, np.ones(5)/5, mode='same')
+
+    # Left side sum negative and right side sum positive
+    new_hist = np.copy(hist)/40000
+    new_hist[:320] = new_hist[:320]
+    new_hist[320:] = -new_hist[320:]
+
+    # Apply 1D gausian kernel
+    steer = np.sum(new_hist)
     steer = np.clip(steer, -1, 1)
-
-    # print(steer)
-    # map to from -1 to 1 to 0 to 1
     steer = (steer + 1)/2
-    # print(steer)
+
     vehicle.run(steer, -0.03)
